3bc44cb28059728921f64ba24137d2311a3e2419
Fedotov Pavel <pfedotov@lohika.com>
2015-11-23 17:53:10 +0200

    Hujak

--- src/com/lohika/tests/LengthWidthTest.java
+++ src/com/lohika/tests/LengthWidthTest.java
@@ -1,17 +1,47 @@
 package com.lohika.tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Pavel on 11/23/15.
  */
 public class LengthWidthTest {
+    private static String[] types = {"smallint"};
+
+    private static String[] values = {"null", "1", "10"};
+
+    private static int[] length = {1, 100, 1000000};
+    private static int[] width = {1, 5, 1012};
 
 
 
-    public static String getTableName(String dataType){
+    private static String getTableName(String dataType){
         return dataType + "WIDTH_LENGTH";
     }
 
-    public static String getTableIndex(int width, int length){
+    private static String getTableIndex(int width, int length){
         return width + "_" + length;
     }
+
+    private static String getColumnIndex(int index){
+        return String.valueOf(index);
+    }
+
+
+    public void createTests(){
+        for (String type: types){
+            for (int wi : width){
+                for (int le : length){
+                    Map<String, String> changes = new HashMap<String, String>();
+                    changes.put("TABLE_NAME", getTableName(type));
+                    changes.put("TABLE_INDEX", getTableIndex(wi, le));
+
+
+                }
+            }
+        }
+
+    }
+
 }
