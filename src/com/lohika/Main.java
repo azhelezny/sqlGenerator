e4814fea8d62c42b126e9c7b3552b057825e52d9
azhelezny <azhelezny@gmail.com>
2015-11-24 12:42:14 +0200

    pish

--- src/com/lohika/Main.java
+++ src/com/lohika/Main.java
@@ -5,11 +5,9 @@ import com.lohika.tests.LengthWidthTest;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         //String pattern = TestPattern.LENGTH_WIDTH_TEST_PATTERN;
         LengthWidthTest.createTests();
-
-
 
 
     }
