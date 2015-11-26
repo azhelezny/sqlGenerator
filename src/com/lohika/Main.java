cf3bd62e1f288926e065fa2bb8015ee5077e06bb
azhelezny <azhelezny@gmail.com>
2015-11-26 20:14:06 +0200

    pish 2

--- src/com/lohika/Main.java
+++ src/com/lohika/Main.java
@@ -1,9 +1,6 @@
 package com.lohika;
 
-import com.lohika.inserts.BigintInsert;
-import com.lohika.tests.LengthWidthTest;
-import com.lohika.tests.MaxWidthTest;
-import com.lohika.tests.constraints.check.ConstraintsUniqueTest;
+import com.lohika.tests.constraints.check.ConstraintsSingleColumnTest;
 import com.lohika.tests.constraints.check.PrimaryKeysMultColumsConstraints;
 import com.lohika.tests.constraints.check.ConstraintsSingleColumnTest;
 
